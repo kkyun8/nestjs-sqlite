cef31424210c45a07c5321f11e4f681f42ebddba
kkyun8 <kim23302@icloud.com>
2021-06-19 11:58:59 +0900

    swagger追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger start
+  const config = new DocumentBuilder()
+    .setTitle('nestjs-sqlite')
+    .setDescription('test')
+    .setVersion('1.0')
+    .addTag('tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // swagger url http://localhost:3000/api/#/
+  // swagger end
+
   await app.listen(3000);
 }
 bootstrap();
